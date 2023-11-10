71acff492dab11d3219dfbd73dca01ae0fb26652
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-11-10 20:48:10 -0300

    :recycle: Atualizado imports

--- formacao-java-developer/atividade/io-java/src/Exercicio1IOCaracter.java
+++ formacao-java-developer/atividade/io-java/src/Exercicio1IOCaracter.java
@@ -4,9 +4,7 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
 public class Exercicio1IOCaracter {
--- formacao-java-developer/atividade/io-java/src/Exercicio3IOCaracter.java
+++ formacao-java-developer/atividade/io-java/src/Exercicio3IOCaracter.java
@@ -6,7 +6,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class Exercicio3IOCaracter {
