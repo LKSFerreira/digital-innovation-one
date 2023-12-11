6f159e01107c1d4ac75f8529534e82d498c59cfa
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-12-11 17:15:46 -0300

    :sparkles: Criada todas as `entidades` necessáriias para continuidade do desafio
    :one: Aplicado todas as annotaions necessarias para esficificar o máximo possível do banco de dados

--- formacao-java-developer/desafios/crud-rest-api-data-jpa/src/main/java/ferreira/crudrestapidatajpa/models/AlunoModel.java
+++ formacao-java-developer/desafios/crud-rest-api-data-jpa/src/main/java/ferreira/crudrestapidatajpa/models/AlunoModel.java
@@ -5,12 +5,45 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // lombok annotation gerencia os getters, setters, equals, hashcode, toString
+@NoArgsConstructor // lombok annotation Obs: O hibernate precisa de um construtor vazio
+@AllArgsConstructor // lombok annotation gera um construtor com todos os atributos da classe
+@Entity // Jakarta Persistence para indicar uma entidade do banco de dados
+@Table(name = "tb_alunos") // Jakarta Persistence para indicar o nome da tabela no banco de dados
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" }) // Jackson para ignorar propriedades do hibernate
 public class AlunoModel {
 
+  @Id // Jakarta Persistence para indicar que o atributo é uma chave primária
+  @GeneratedValue(generator = "UUID") // Jakarta Persistence para indicar que o atributo é gerado automaticamente
   private UUID id;
+
+  @Column(nullable = false, length = 100) // Características da coluna no db.
   private String nome;
+
+  @Column(nullable = false, length = 11, unique = true) // Características da coluna no db.
   private String cpf;
-  private String bairro;
+
+  @Column(nullable = false, length = 10, name = "data_nascimento") // Características da coluna no db.
+  @DateTimeFormat(pattern = "dd-MM-yyyy")
   private LocalDate dataNascimento;
+
+  @OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY) // Jakarta Persistence para indicar que o atributo é uma relação de um para muitos
+  @JsonIgnore
   private List<AvaliacaoFisicaModel> avaliacoesFisicas = new ArrayList<>();
 }
--- formacao-java-developer/desafios/crud-rest-api-data-jpa/src/main/java/ferreira/crudrestapidatajpa/models/MatriculaModel.java
+++ formacao-java-developer/desafios/crud-rest-api-data-jpa/src/main/java/ferreira/crudrestapidatajpa/models/MatriculaModel.java
@@ -3,8 +3,36 @@ package ferreira.crudrestapidatajpa.models;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_matriculas")
 public class MatriculaModel {
+
+  @Id
+  @GeneratedValue(generator = "UUID")
   private UUID id;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "aluno_id", nullable = false)
   private AlunoModel aluno;
-  private LocalDateTime dataDaMatricul = LocalDateTime.now();
+
+  @CreationTimestamp
+  @Column(name = "data_matricula", nullable = false)
+  @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+  private LocalDateTime dataDaMatricula;
 }
